9eedfb0e9cacad179d9a6bda51813060af6c63d6
lina1989 <mampokolina@gmail.com>
2023-07-26 14:08:25 +0200

    finalisation du projet

--- assets/script.js
+++ assets/script.js
@@ -17,16 +17,69 @@ const slides = [
 	}
 
 ]
+// Définir une variable pour suivre le diaporama actuel
+let currentSlide = 0;
 
+// Fonction pour changer le dot selected en fonction du diaporama actuel
+function changeSelectedDot(){
+	// Sélectionne tous les dots
+	const dots = document.querySelectorAll('.dot')
+
+	// Parcourt chaque dot
+	dots.forEach((dot, idx) => {
+		// Enlève la classe de dot_selected
+		dot.classList.remove('dot_selected')
+		// Si l'indice du dot correspond au diaporama actuel, ajoute la classe de dot_selected
+		if(idx === currentSlide){
+			dot.classList.add('dot_selected')
+		}
+	});
+}
+
+// Fonction pour changer de diaporama
+function changeSlide(){
+	// Obtient le diaporama actuel de la liste de diaporamas
+	const slide = slides[currentSlide]
+	// Sélectionne l'image de la bannière et le texte d'accompagnement
+	const banner = document.querySelector('.banner-img')
+	const tagLine = document.querySelector('#banner p')
+
+	// Met à jour l'image de la bannière et le texte d'accompagnement avec ceux du diaporama actuel
+	banner.setAttribute('src', `./assets/images/slideshow/${slide['image']}`)
+	tagLine.innerHTML = slide['tagLine']
+}
+
+// Sélectionne les flèches de navigation
 const arrow_left = document.querySelector('.arrow_left');
 const arrow_right = document.querySelector('.arrow_right');
 
+// Ajoute un événement de clic à la flèche gauche
 arrow_left.addEventListener('click', function() {
-    alert('Clic sur le bouton gauche');
+	// Si on est au premier diaporama, va au dernier
+    if(currentSlide === 0){
+		currentSlide = slides.length - 1
+	} else {
+		// Sinon, va au diaporama précédent
+    	currentSlide -= 1
+	}
+	// Appelle les fonctions pour changer de diaporama et changer le point sélectionné
+	changeSlide()
+	changeSelectedDot()
 });
 
+// Ajoute un événement de clic à la flèche droite
 arrow_right.addEventListener('click', function() {
-    alert('Clic sur le bouton droit');
+	// Si on est au dernier diaporama, revient au premier
+    if(currentSlide === slides.length - 1){
+		currentSlide = 0
+	} else {
+		// Sinon, passe au diaporama suivant
+    	currentSlide += 1
+	}
+
+	// Appelle les fonctions pour changer de diaporama et changer le point sélectionné
+	changeSlide()
+	changeSelectedDot()
 });
 
 
